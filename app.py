3bc33ca00be352fc60eea8a796583a5d61d8829a
Mark Schnabel <markschnabelcs@gmail.com>
2019-02-12 15:46:28 -0500

    Added url route
    	In this commit I added a url route for users to submit direct
    	links of images and recieve a color palette. Works almost the
    	same way as the upload route, but just does some extra
    	pre-processing to extract the image from the web and use it. I
    	also added flask CORS to enable CORS within the application for
    	development.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
 import numpy as np 
 import cv2 
+import urllib
 from palette import preprocess, generate_palettes 
 
 app = Flask(__name__) 
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+
 
 def validate_extension(filename):
     """Helper function for validating that the file provided by the user is 
@@ -14,41 +18,81 @@ def validate_extension(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/api/generate_palettes/upload', methods=["POST"])
-def generate_palettes_from_upload():
-    """Route for generating color palettes via direct upload."""   
 
-    # Validate the uploaded file:
+@app.route('/api/get_palettes/upload', methods=["POST"])
+def get_palettes_from_upload():
+    """Route for generating color palettes via direct upload."""
     if not request.files:
         return make_response(jsonify({'error': 'File required.'}), 400)
+        
     if 'image' not in request.files:
-        return make_response(jsonify({'error': 'Image field was not provided'}), 400)
-    
+        return make_response(jsonify({'error': 'No image was provided'}), 400)
+
     image = request.files['image']
 
     if not validate_extension(image.filename):
         return make_response(jsonify(
-            {'error': 'It seems the image you\'ve submitted is invalid. Please make ' 
-            + 'sure the image uses on of the following extensions .jpg, jpeg or .png.'}), 
+            {'error': 'It seems the image you\'ve submitted is invalid. Please make sure the image uses on of the following extensions .jpg, jpeg or .png.'}), 
             400
         )
 
     try:
-        # Try to pre-process the image, if this succeeds the input was valid and
-        # the result can be directly passed to the generate_palette function 
-        # since it will only be working with properly formatted numpy arrays from 
-        # here on. If it fails the input was not valid.
-        preprocessed_image = preprocess(image)
+        # Try to pre-process the image, if this succeeds the rest of the palette
+        # generation should go smoothly since it will only be working with properly
+        # formatted numpy arrays, if not the entire process will fail.
+        image = preprocess(image)
     except:
         return make_response(
-            jsonify(
-                {"error": "The uploaded image could not be read."}), 
-                400
-            )
+            jsonify({"error": "The uploaded image could not be read."}), 
+            400
+        )
 
-    color_palette = generate_palettes(preprocessed_image)
+    palettes = generate_palettes(image)
 
-    return make_response(jsonify(color_palette), 200)
+    return make_response(jsonify(palettes), 200)
 
-if __name__ == '__main__': 
+
+@app.route('/api/get_palettes/url', methods=['POST'])
+def get_palettes_from_url():
+    """Route for generating color palettes provided a direct link to an image."""
+    if not request.get_json():
+        return make_response(jsonify({'error': 'JSON data required.'}), 400)
+
+    if 'url' not in request.get_json():
+        return make_response(jsonify({'error': 'No URL was provided.'}), 400)
+    
+    url = request.get_json()['url']
+
+    if not validate_extension(url):
+        return make_response(jsonify(
+            {'error': 'It seems the URL you\'ve submitted is invalid. Please make '
+            + 'sure it leads directly to an image and ends in either: .jpg, jpeg or .png.'}), 
+            400
+        )
+
+    try:
+        image = urllib.request.urlopen(url)
+    except:
+        return make_response(
+            jsonify({"error": "Could not retrieve the image from the provided URL. "
+            + "The server hosting it may not allow external requests. Please try again."}), 
+            400
+        )
+
+    try:
+        # Try to pre-process the image, if this succeeds the rest of the palette
+        # generation should go smoothly since it will only be working with properly
+        # formatted numpy arrays, if not the entire process will fail.
+        image = preprocess(image)
+    except:
+        return make_response(
+            jsonify({"error": "The image retrieved from the URL could not be read."}), 
+            400
+        )
+
+    palettes = generate_palettes(image)
+
+    return make_response(jsonify(palettes), 200)
+
+if __name__ == '__main__':
     app.run(debug=True)
