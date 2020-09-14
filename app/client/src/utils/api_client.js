352b94e5a43b576711850ebe33aa829eb42ecb7a
Mark <mark.schnabel@markschnabel.com>
2020-09-14 02:42:54 -0400

    Remove default headers on axios client
    this would be useful if this app was going to make more than one request
    and that request wasnt a multipart/form-data request, but sadly that is not
    the case for this app

--- app/client/src/utils/api_client.js
+++ app/client/src/utils/api_client.js
@@ -1,8 +1,3 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: '/api',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
+export default axios.create({ baseURL: '/api' });
