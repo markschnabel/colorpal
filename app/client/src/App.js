6e795e48e1e0a7d92414f8f77b6cd27549090105
Mark <mark.schnabel@markschnabel.com>
2020-09-13 21:41:54 -0400

    Setup basic routing for the client app

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Layout from './components/layout';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
