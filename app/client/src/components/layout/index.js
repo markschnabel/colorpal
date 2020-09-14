267aa9149f7c1c7658f69dd3e6181206c73d3f0b
Mark <mark.schnabel@markschnabel.com>
2020-09-13 21:59:50 -0400

    Wrap the app in a basic container

--- app/client/src/components/layout/index.js
+++ app/client/src/components/layout/index.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+import Container from '../shared/Container';
+
+const Content = styled.div`
+  min-height: 100vh;
+  height: 100%;
+  padding-bottom: 60px;
+`;
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      {children}
-    </div>
+    <Container>
+      <Content>
+        {children}
+      </Content>
+    </Container>
   );
 };
 
---
+++ app/client/src/components/shared/Container.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 1240px;
+  margin: 0 auto;
+  padding: 0 20px;
+
+  @media only screen and (max-width: 1240px) {
+    margin: 0 5%;
+    padding: 0;
+  }
+`
+
+export default Container;
