695d4aa704049c82fb212c548889cae659bd945a
Mark <mark.schnabel@markschnabel.com>
2020-09-14 19:54:11 -0400

    Quick styling for the 404 page

--- app/client/src/pages/NotFound.js
+++ app/client/src/pages/NotFound.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { RiFileSearchLine } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NotFoundContainer = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 50vh;
+
+  p {
+    margin: 24px auto;
+  }
+
+  a {
+    margin-bottom: 24px;
+  }
+`
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>NotFound</h1>
-    </div>
+    <NotFoundContainer>
+      <h1>Not Found</h1>
+      <p>We're sorry, we couldn't find the page you were looking for.</p>
+      <Link to="/">Click here to go back home</Link>
+      <RiFileSearchLine size={200} />
+    </NotFoundContainer>
   )
 }
 
