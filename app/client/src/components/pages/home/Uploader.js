7831db382c4fc0ec7a3df528f5b35655b9b3b505
Mark <mark.schnabel@markschnabel.com>
2020-09-14 19:20:11 -0400

    WIP Making fetching the platte of a newly uploaded image work

--- app/client/src/components/pages/home/Uploader.js
+++ app/client/src/components/pages/home/Uploader.js
@@ -1,7 +1,7 @@
-import React, { Component, Fragment } from "react";
-import Dropzone from "react-dropzone";
+import React from 'react';
+import Dropzone from 'react-dropzone';
 import { AiOutlineCloudUpload } from 'react-icons/ai';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const UploadButton = styled.button`
   width: 400px;
@@ -38,67 +38,48 @@ const MAX_FILE_SIZE = 5000000; // 5 mb
 
 const dropzoneRef = React.createRef();
 
-class Uploader extends Component {
-  handleClick = e => {
+const Uploader = (props) => {
+  const onClick = (e) => {
     e.preventDefault();
   };
 
-  handleDrop = (files, rejectedFiles) => {
-    if (files.length + rejectedFiles.length > 1) {
-      this.props.setError("Only one file can be processed at a time.");
-      return;
-    }
+  const onDrop = (acceptedFiles, rejectedFiles) => {
+    const data = new FormData();
+    data.append('image', acceptedFiles[0]);
 
-    if (rejectedFiles.length >= 1) {
-      if (rejectedFiles[0].size > MAX_FILE_SIZE) {
-        this.props.setError("Files must not exceed 5MB.");
-        return;
-      }
-
-      if (!ACCEPTED_FILE_TYPES.split(",").includes(rejectedFiles[0].type)) {
-        this.props.setError("Files must end in one of the following extensions: .jpg, .jpeg, or .png.");
-        return;
-      }
-    }
-
-    const formData = new FormData();
-    formData.append("image", files[0]);
-
-    this.props.fetchResults(formData, files[0], 'upload');
+    props.extractPalette(data);
   };
 
-  render() {
-    return (
-      <Fragment>
-        <Dropzone
-          multiple={false}
-          accept={ACCEPTED_FILE_TYPES}
-          maxSize={MAX_FILE_SIZE}
-          onDrop={this.handleDrop}
-          onClick={this.handleClick}
-          ref={dropzoneRef}
-        >
-          {({ getRootProps, getInputProps, isDragActive }) => {
-            return (
-              <DragAndDropContainer isDragActive={isDragActive} {...getRootProps()}>
-                <AiOutlineCloudUpload style={{ fontSize: '64px' }} />
-                <p style={{ margin: '12px' }}>Drag and drop an image here</p>
-                <UploadButton onClick={() => dropzoneRef.current.open()}>
-                  Manual upload
-                </UploadButton>
-                <p>
-                  Images must be less than 5MB and use one of the following
-                  extensions: <strong>.jpg</strong>, <strong>.jpeg</strong> or
-                  <strong>.png</strong>
-                </p>
-                <input {...getInputProps()} />
-              </DragAndDropContainer>
-            );
-          }}
-        </Dropzone>
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Dropzone
+      multiple={false}
+      accept={ACCEPTED_FILE_TYPES}
+      maxSize={MAX_FILE_SIZE}
+      onDrop={onDrop}
+      onClick={onClick}
+      ref={dropzoneRef}
+    >
+      {({ getRootProps, getInputProps, isDragActive }) => (
+        <DragAndDropContainer {...getRootProps()} isDragActive={isDragActive}>
+          <AiOutlineCloudUpload style={{ fontSize: '64px' }} />
+          <p style={{ margin: '12px' }}>
+            Drag and drop an image here
+          </p>
+          <UploadButton onClick={() => dropzoneRef.current.open()}>
+            Manual upload
+          </UploadButton>
+
+          <p>
+            Images must be less than 5MB and use one of the following
+            extensions: <strong>.jpg</strong>, <strong>.jpeg</strong> or
+            <strong>.png</strong>
+          </p>
+
+          <input {...getInputProps()} />
+        </DragAndDropContainer>
+      )}
+    </Dropzone>
+  );
+};
 
 export default Uploader;
--- app/client/src/pages/Home.js
+++ app/client/src/pages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { extractPalette } from '../actions/palette';
+import { WaveSpinner } from 'react-spinners-kit';
 
 import Uploader from '../components/pages/home/Uploader';
 
@@ -16,17 +19,34 @@ const DescriptionText = styled.p`
   margin-top: 20px;
 `;
 
-const Home = () => {
+const Home = ({ extractPalette, palette }) => {
   return (
     <HomePageContainer>
       <h1>Colorpal</h1>
+
       <DescriptionText>
         ColorPal is a tool that allows you to turn images into color palettes for whatever project you may want to use them for.
         Simply submit an image down below and we'll extract it's primary colors and create your perfect palette.
       </DescriptionText>
-      <Uploader />
+
+      {(() => {
+        if (palette.loading)
+          return <WaveSpinner size={125} style={{ margin: '32px auto' }} color="#686769" />
+
+        if (!palette.palette)
+          return <Uploader extractPalette={extractPalette} />
+
+        return <h1>{palette.palette.map(p => JSON.stringify(p)).join(', ')}</h1>
+      })()}
+
     </HomePageContainer>
   )
 }
 
-export default Home
+const mapStateToProps = state => ({
+  palette: state.palette,
+  loading: state.loading,
+  errors: state.errors,
+});
+
+export default connect(mapStateToProps, { extractPalette })(Home)
