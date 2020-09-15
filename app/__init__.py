7d1362d0351efc45e2ff779553579b48be0d242e
Mark <mark.schnabel@markschnabel.com>
2020-09-15 02:33:52 -0400

    Make sure all unregistered routes get sent to the react app

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, static_folder='./client/build', static_url_path='/')
-
-    @app.route('/')
-    def index():
-        return app.send_static_file('index.html')
+    app = Flask(__name__, static_folder='./client/build')
 
     # Make sure all blueprints are imported AFTER the app has been created
     from .api import api_blueprint
     app.register_blueprint(api_blueprint)
+
+    # Register this route last - it's a catch all route that points to the client app
+    # and other routes need to be registered earlier to take precedence
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def index(path):
+        return app.send_static_file('index.html')
 
     return app
