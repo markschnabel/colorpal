8121a4b152293f3be5993dea2eab2b043c056a29
Mark <mark.schnabel@markschnabel.com>
2020-09-15 01:03:33 -0400

    Serve the client app from flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='./client/build', static_url_path='/')
 
+    @app.route('/')
+    def index():
+        return app.send_static_file('index.html')
+
+    # Make sure all blueprints are imported AFTER the app has been created
     from .api import api_blueprint
     app.register_blueprint(api_blueprint)
 
